73ddc2d53222988c3605f51fc001f3902969596a
Bharath Ravelli <88048287+bharathravelli-419@users.noreply.github.com>
2022-03-19 00:08:32 +0530

    Tayler's Series using Horner's rule O(n) TC

--- Recursion.cpp
+++ Recursion.cpp
@@ -3,18 +3,12 @@ using namespace std;
 
 double e(int x,int n)
 {
-    static double p=1,f=1;
-    double r;
-    if(n==0)
-        return 1;
-    else
-    {
-        r = e(x,n-1);
-        p=p*x;
-        f= f*n;
-        return r+p/f;
+    static double s =1;
+   if(n==0)
+    return s;
+   s = (s*x/n) + 1;
+   return e(x,n-1);
 
-    }
 }
 int main()
 {
