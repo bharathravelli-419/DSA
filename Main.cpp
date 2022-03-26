50929804804616401f20446fb947515a6eb04f20
Bharath Ravelli <88048287+bharathravelli-419@users.noreply.github.com>
2022-03-26 22:21:31 +0530

    Kadane's Algo for max. sum sub array

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,51 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    int sum =0;
-    int maxi = arr[0];
-    for(int i=0;i<n;i++)
-    {
-        int sum =0;
-        for(int j =i;j<n;j++)
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+
+        // Your code here
+        long long sum =0;
+        long long max = INT_MIN;
+        for(int i=0;i<n;i++)
         {
-            sum = sum+arr[j];
-            maxi = sum>maxi? sum:maxi;
+            sum = sum+arr[i];
+            if(sum>max)
+            max = sum;
+            if(sum<0)
+            sum=0;
         }
+        return max;
 
     }
-    cout<<maxi;
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+
+        cin>>n; //input size of array
+
+        int a[n];
+
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+
+        Solution ob;
+
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
 }
+  // } Driver Code Ends
