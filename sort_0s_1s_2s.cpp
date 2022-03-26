61355b6ca0e6295744ca1b296c418f1c789ba057
Bharath Ravelli <88048287+bharathravelli-419@users.noreply.github.com>
2022-03-26 19:01:21 +0530

    Sorting 0s 1s 2s with O(n) TC

---
+++ sort_0s_1s_2s.cpp
@@ -0,0 +1,71 @@
+##include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // coode here
+        int mid,low,high;
+        mid = low = 0;
+        high = n-1;
+        int temp;
+        while(mid<=high)
+        {
+            if(a[mid ]==0)
+            {
+                temp = a[mid];
+                a[mid] = a[low];
+                a[low] = temp;
+                low++;
+                mid++;
+            }
+           else if(a[mid]==1)
+             mid++;
+            else if(a[mid] ==2)
+            {
+                temp = a[mid];
+                a[mid] =a[high];
+                a[high] = temp;
+                high--;
+            }
+
+        }
+
+    }
+
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+
+
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
